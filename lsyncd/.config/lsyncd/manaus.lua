9ccb71f78ebaaadc2e63af6fb6e041d5ce565d88
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2016-12-24 08:31:22 -0200

    lsyncd: new manaus file and add post function

---
+++ lsyncd/.config/lsyncd/manaus.lua
@@ -0,0 +1,39 @@
+settings {
+	logfile    = "/tmp/lsyncd_manaus.log",
+	statusFile = "/tmp/lsyncd_manaus.status",
+	nodaemon   = false,
+}
+
+sync {
+	default.rsync,
+	source = "/home/pfsmorigo",
+	target = "/mnt/disk/dublin-home/pfsmorigo",
+	exclude = { '~/Private' },
+
+	collect = function(agent, exitcode)
+		local config = agent.config
+		local rc = config.exitcodes and config.exitcodes[exitcode]
+
+		if rc == "die" then
+			return rc
+		end
+
+		if agent.isList then
+			if rc == "again" then
+				log("Normal", "XXX Retrying a list on exitcode = ",exitcode)
+			else
+				log("Normal", "XXX Finished a list = ",exitcode)
+				--spawnShell('[[ /usr/bin/echo "$1" > /tmp/fil ]]', event.sourceName)
+			end
+		else
+			if rc == "again" then
+				log("Normal", "XXX Retrying ",agent.etype,
+				" on ",agent.sourcePath," = ",exitcode)
+			else
+				log("Normal", "XXX Finished ",agent.etype,
+				" on ",agent.sourcePath," = ",exitcode)
+			end
+		end
+		return rc
+	end,
+}
