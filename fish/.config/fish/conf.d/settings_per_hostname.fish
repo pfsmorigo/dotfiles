494704db8aa59c8ca37f5466511e93d43d279c1f
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2024-12-28 06:08:01 -0300

    fish: small changes
    Signed-off-by: Paulo Flabiano Smorigo <pfsmorigo@gmail.com>

--- fish/.config/fish/conf.d/settings_per_hostname.fish
+++ fish/.config/fish/conf.d/settings_per_hostname.fish
@@ -1,6 +1,6 @@
 switch (hostname)
 	case ubuntu-jammy
 		set -g theme_display_user no
-		set -g theme_display_hostname no
+		#set -g theme_display_hostname no
 	case '*'
 end
---
+++ fish/.config/fish/functions/full_prompt.fish
@@ -0,0 +1,5 @@
+function full_prompt
+	set -g theme_display_git $argv[1]
+	set -g theme_display_hostname $argv[1]
+	set -g theme_display_screen $argv[1]
+end
