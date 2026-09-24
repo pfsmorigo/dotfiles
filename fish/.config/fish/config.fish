2e789e9f38689f141e3eeedfbccb0ebf280dfcbf
Paulo Flabiano Smorigo <pfsmorigo@gmail.com>
2026-09-23 21:23:55 -0300

    fish and tmux configs. add more words to spell dict
    Signed-off-by: Paulo Flabiano Smorigo <pfsmorigo@gmail.com>

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+	set -g theme_date_timezone America/Sao_Paulo
+	set -g theme_date_format "+%H:%M:%S"
+	set -g theme_show_exit_status yes
+	set -g theme_title_use_abbreviated_path yes
+	set -g theme_nerd_fonts yes
+	set -g theme_color_scheme dark
+	set -g theme_display_cmd_duration yes
+	set -g theme_display_git yes
+	set -g theme_display_git_ahead_verbose yes
+	set -g theme_display_git_dirty_verbose yes
+	set -g theme_display_hostname yes
+	set -g theme_display_jobs_verbose yes
+	set -g theme_display_screen yes
+	set -g theme_display_screen_verbose yes
+	set -g theme_display_tmux yes
 end
--- fish/.config/fish/functions/t.fish
+++ fish/.config/fish/functions/t.fish
@@ -1,3 +1,31 @@
-function t --wraps='tmux attach 2>/dev/null || tmux new-session \\; choose-tree -s' --wraps='tmux new-session \\; choose-tree -s' --description 'alias t=tmux new-session \\; choose-tree -s'
-    tmux new-session \; choose-tree -s $argv
+function t --description "Smart tmux session manager"
+    # Prevent running tmux inside an existing tmux session
+    if set -q TMUX
+        echo "Already inside a tmux session."
+        return 1
+    end
+
+    if test (count $argv) -gt 0
+        set -l session_name $argv[1]
+
+        # Use current folder name if argument is "."
+        if test "$session_name" = "."
+            set session_name (path basename (pwd))
+        end
+
+        # Sanitize dots in session names (tmux treats dots as session:window separators)
+        set session_name (string replace -a '.' '_' $session_name)
+
+        # Attach to the session if it exists, otherwise create it
+        tmux new-session -A -s "$session_name"
+    else
+        # If any tmux sessions exist, launch directly into choose-tree
+        if tmux has-session 2>/dev/null
+            tmux new-session -d 2>/dev/null
+            tmux attach-session \; choose-tree -s
+        else
+            # No sessions exist; create a fresh default session
+            tmux new-session
+        end
+    end
 end
